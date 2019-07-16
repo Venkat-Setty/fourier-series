fbd094b9298ee2e8c10bf50ec05cc5242419f156
Setty, Venkat <phani.setty@cbre.com>
2019-07-16 11:53:13 -0500

    drawing the sine wave

--- app.js
+++ app.js
@@ -1,46 +1,65 @@
  var radius = 80;
-    var point_size = 2;
-    var center_x = 350;
-    var center_y = 150;
+ var point_size = 2;
+ var center_x = 100;
+ var center_y = 100;
+ var sine_x0 = 200;
+ var sine_x1;
+ var sine_y0;
+ var sine_y1;
+ var offset = 100;
 
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
 
-    function drawCircle() {
-        ctx.beginPath();
-        ctx.arc(center_x, center_y, radius, 0, 2 * Math.PI);
-        ctx.stroke();
-    }
+ var c = window.document.getElementById("myCanvas");
+ var ctx = c.getContext("2d");
 
-    function drawPoint(angle) {
-        var x = center_x + radius * Math.cos(-angle * Math.PI / 180);
-        var y = center_y + radius * Math.sin(-angle * Math.PI / 180);
+ function drawCircle() {
+     ctx.beginPath();
+     ctx.arc(center_x, center_y, radius, 0, 2 * Math.PI);
+     ctx.stroke();
+ }
 
-        ctx.beginPath();
-        ctx.arc(x, y, point_size, 0, 2 * Math.PI);
-        ctx.fillStyle = "#ff0000";
+ function drawPoint(angle) {
+     var x = center_x + radius * Math.cos(-angle * Math.PI / 180);
+     var y = center_y + radius * Math.sin(-angle * Math.PI / 180);
+   
+     sine_y0 = y;
+     sine_x1 = sine_x0 + 0.2;
+     sine_x0 += 0.2;
+     sine_y1 = center_y + radius * Math.sin(-(angle + 0.2) * Math.PI / 180);
+     ctx.beginPath();
+     ctx.arc(x, y, point_size, 0, 2 * Math.PI);
+     ctx.fillStyle = "#ff0000";
 
-        ctx.fill();
-        
-        ctx.beginPath();
-        ctx.moveTo(center_x, center_y);
-        ctx.lineTo(x, y);
-        ctx.stroke();
-        ctx.closePath();
-        
-    }
+     ctx.fill();
 
-    //Execution
-        drawCircle();
-        var Angle = 0;
+     ctx.beginPath();
+     ctx.moveTo(center_x, center_y);
+     ctx.lineTo(x, y);
+     ctx.stroke();
+     ctx.closePath();
 
-    function draw() {
-        ctx.clearRect(0, 0, 500, 500);
-        drawCircle();
-        drawPoint(Angle);
-        Angle += 0.5;
-        window.requestAnimationFrame(draw);
-    }
-    draw();
-    //window.requestAnimationFrame(draw);
-    //we keep increasing the angle in an interval and point moves in 
+ }
+
+ function drawSine() {
+     ctx.beginPath();
+     ctx.moveTo(sine_x0, sine_y0);
+     ctx.lineTo(sine_x1, sine_y1);
+     ctx.stroke();
+
+ }
+
+ //Execution
+ drawCircle();
+ var Angle = 0;
+
+ function draw() {
+     ctx.clearRect(0, 0, 200, 500);
+     drawCircle();
+     drawPoint(Angle);
+     drawSine();
+     Angle += 0.2;
+     window.requestAnimationFrame(draw);
+ }
+ draw();
+ //window.requestAnimationFrame(draw);
+ //we keep increasing the angle in an interval and point moves in 
